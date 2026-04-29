abf86f970745ef20d9d612718bd26a5fad0af603
omkarchavan4170-alt <omkarchavan4170@gmail.com>
2026-04-30 00:19:42 +0530

    Delete app/model.py

--- app/model.py
+++
@@ -1,8 +0,0 @@
-def predict_aqi(data):
-    aqi = (
-        0.4 * data["pm25"] +
-        0.3 * data["pm10"] +
-        0.2 * data["no2"] +
-        0.1 * data["co"]
-    )
-    return round(aqi, 2)
